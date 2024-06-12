eddd75f5d0c983f36be7f7ed2f221413cbcea58a
Tomasz <tomasz.goluch@gmail.com>
2024-06-12 22:23:47 +0200

    generated migrations

--- src/Infrastructure/DAL/Extensions.cs
+++ src/Infrastructure/DAL/Extensions.cs
@@ -10,6 +10,17 @@ namespace Infrastructure.DAL
         private const string OptionsSectionName = "MariaDB";
         public static void AddMariaDB(this IServiceCollection services, IConfiguration configuration)
         {
+            // komendy do dla KMP:
+            // usuwanie bazy danych:
+            //   - Drop-Database -Context IdentityContext -Project Infrastructure
+            // tworzenia migracji:
+            //   - Add-Migration InitIdentityData -Context IdentityContext -Project Infrastructure
+            // aktualizacji bazy danych na podstawie migracji:
+            //   - Update-Database InitIdentityData -Context IdentityContext -Project Infrastructure
+            // usunięcia ostatniej migracji:
+            //   - Add-Migration -Context IdentityContext -Project Infrastructure
+
+
             services.Configure<MariaDBOptions>(configuration.GetRequiredSection(OptionsSectionName));
             var mariaDBOptions = configuration.GetOptions<MariaDBOptions>(OptionsSectionName);
             var serverVersion = new MariaDbServerVersion(new Version(10, 3, 0));

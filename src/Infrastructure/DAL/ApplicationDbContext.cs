23e1ccec369265c285c92f418d3e9f89e8779656
Tomasz <tomasz.goluch@gmail.com>
2024-06-14 00:00:28 +0200

    moved adding contexts to Infrastructure

---
+++ src/Infrastructure/DAL/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.DAL
+{
+    internal class ApplicationDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+        }
+    }
+}
